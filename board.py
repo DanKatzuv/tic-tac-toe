94666b268eceba546d83c15cbbd1cc784f37f486
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-02 15:26:48 +0300

    Create init method

--- board.py
+++ board.py
@@ -1,2 +1,8 @@
 class Board(object):
     EMPTY = ' '
+
+    def __init__(self):
+        # self.board = [3 * [' ']]
+        self.board = [['X', 'O', 'X'],
+                      ['X', 'O', 'X'],
+                      ['X', 'O', 'X']]
