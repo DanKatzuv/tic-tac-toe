9a4421d3560410765b10a4c831c54e72c8253bec
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-02 15:27:29 +0300

    Create board file

--- board.py
+++ board.py
@@ -6,3 +6,58 @@ class Board(object):
         self.board = [['X', 'O', 'X'],
                       ['X', 'O', 'X'],
                       ['X', 'O', 'X']]
+
+    def __str__(self):
+        return ''.join(' '.join(row) + '\n' for row in self.board)
+
+    def turn(self, player, row, column):
+        self._insert(player, row, column)
+        return self._is_winner(row, column)
+
+    def _insert(self, player, row, column):
+        self._raise_if_cell_not_empty(row, column)
+        self.board[row][column] = player
+
+    def _is_winner(self, row, column):
+        for row in self.board:
+            if len(set(row)) == 1:
+                return True
+        for column in self.board:
+            if len(set(column)) == 1:
+                return True
+        main_diagonal = [self.board[i][i] for i in xrange(3)]
+        secondary_diagonal = [self.board[i][2 - i] for i in xrange(3)]
+        return any(len(set(diagonal)) == 1 for diagonal in (main_diagonal, secondary_diagonal))
+
+    def _raise_if_cell_not_empty(self, row, column):
+        if self.board[row][column] != self.EMPTY:
+            raise NotEmptyCellError(row, column)
+
+    def _raise_if_cell_not_in_bounds(self, row, column):
+        try:
+            self.board[row][column]
+        except IndexError:
+            raise NotEmptyCellError(row, column)
+
+
+class OutOfBoundsError(IndexError):
+    def __init__(self, row, column):
+        super(IndexError, self).__init__()
+        self.row = row
+        self.column = column
+
+
+class NotEmptyCellError(Exception):
+    def __init__(self, row, column):
+        super(NotEmptyCellError, self).__init__()
+        self.row = row
+        self.column = column
+
+
+def main():
+    board = Board()
+    print board
+
+
+if __name__ == '__main__':
+    main()
