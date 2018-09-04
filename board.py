056c1c008f92432de5e31950623a36bfd840f104
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-05 00:02:25 +0300

    Add method for checking if the board is full

--- board.py
+++ board.py
@@ -13,6 +13,8 @@ class Board(object):
     def turn(self, player, row, column):
         self._insert(player, row, column)
         return self._is_winner(row, column)
+    def is_full(self):
+        return all(self.EMPTY not in row for row in self.board)
 
     def _insert(self, player, row, column):
         self._raise_if_cell_not_empty(row, column)
