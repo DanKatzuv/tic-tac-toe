d8b115a8c3c64d5c8a23ad4dd9fddef21c2cb861
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-05 00:39:23 +0300

    Add function for PvP mode

--- game.py
+++ game.py
@@ -16,9 +16,24 @@ def pvp():
     for player in cycle(('X', 'O')):
         print(board)
         while True:
+            try:
+                row, column = cell_input(player)
+                if board.move(player, row, column):
+                    print(board)
+                    print_win(player)
                 return
+                break
+            except OutOfBoundsError as error:
+                print(f'The cell inputted at row {error.row + 1}, column {error.column + 1} is out of bounds.')
+            except FullCellError as error:
+                print(f'The cell inputted at row {error.row + 1}, column {error.column + 1} is already full.')
+            except Exception as error:
+                print(error)
 
+            if board.is_full():
+                print(board)
                 print_game_over()
+                return
 
 
                 return
