b83df35baa11420e4229464ea11cfaba441b0fbf
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-19 21:18:58 -0600

    Remove Player abstract class from game file and add it to a unique file

---
+++ player.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Player(ABC):
+    """Abstract class that represents a player."""
+
+    def __init__(self, mark):
+        """
+        Instantiate a player.
+
+        :param mark: the mark of the player (X, O)
+        :type mark: str
+        """
+        self.mark = mark
+
+    @abstractmethod
+    def turn(self, board):
+        """
+        Make a turn.
+
+        :param board: the current game's board
+        :type board: Board
+        :return: choice of player
+        :rtype: tuple
+        """
+        pass
