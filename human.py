8721f2b2a756d6cd3592acf9ed05483d1a9cb06b
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-20 20:11:49 -0600

    Add input checking for turn of Human

--- human.py
+++ human.py
@@ -22,10 +22,9 @@ class Human(Player):
         :return: choice of player
         :rtype: tuple
         """
-        print(board)
-        return self.cell_input()
+        return self.cell_input(board)
 
-    def cell_input(self):
+    def cell_input(self, board):
         """
         Input a column number from player.
 
@@ -34,6 +33,15 @@ class Human(Player):
         : return: column number player inputted
         : rtype: tuple
         """
-        row = int(input(f'Player {self.mark}, enter a row number between 1 and 3: ')) - 1
-        column = int(input(f'Player {self.mark}, enter a column number between 1 and 3: ')) - 1
-        return row, column
+        while True:
+            print(board)
+            row = int(input(f'Player {self.mark}, enter a row number between 1 and 3: ')) - 1
+            column = int(input(f'Player {self.mark}, enter a column number between 1 and 3: ')) - 1
+
+            if not board.is_cell_empty(row, column):
+                print('This cell is not empty')
+                continue
+            if not (0 <= row < 3 or 0 <= column < 3):
+                print('This cell is out of bounds')
+                continue
+            return row, column
