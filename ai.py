2deae2e5eb82e537b8cdb6ef37d7f8ae8c7cf92f
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-21 19:48:57 -0600

    Create file and class for AI player

---
+++ ai.py
@@ -0,0 +1,16 @@
+from player import Player
+
+
+class AI(Player):
+    """Class that represents an AI player."""
+
+    def turn(self, board):
+        """
+        Make a turn.
+
+        :param board: the current game's board
+        :type board: Board
+        :return: choice of player
+        :rtype: tuple
+        """
+        pass
