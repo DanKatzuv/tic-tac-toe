0a4e3e0fba6e1b19a6589a44fb13d4236bda88ed
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-25 16:23:54 -0400

    Add test for RandomComputer

---
+++ tests/test_random_computer.py
@@ -0,0 +1,17 @@
+from pytest import mark
+
+from board import Board
+from players import RandomComputer
+
+
+@mark.parametrize('mark', ('X', 'O'))
+def test_row(mark):
+    board = Board()
+    board._board = [[' ', ' ', ' '],
+                    [' ', mark, mark],
+                    [' ', mark, ' ']]
+
+    for _ in range(10):
+        player = RandomComputer(mark)
+        row, column = player.turn(board)
+        assert board.is_cell_empty(row, column)
