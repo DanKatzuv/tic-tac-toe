90c18d8c951d883de0c876df2dccfc8a935461be
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-26 22:10:35 -0400

    Move row and column input from Human into another method

--- players/human.py
+++ players/human.py
@@ -15,13 +15,33 @@ class Human(Player):
         """
         while True:
             print(board)
-            row = int(input(f'Player {self.mark}, enter a row number between 1 and 3: ')) - 1
-            column = int(input(f'Player {self.mark}, enter a column number between 1 and 3: ')) - 1
+            row = self._get_user_input('row')
+            column = self._get_user_input('column')
 
             if not board.is_cell_empty(row, column):
                 print(f'The cell at row {row}, column {column} is not empty')
                 continue
-            if not (0 <= row < 3 or 0 <= column < 3):
-                print(f'The cell at row {row}, column {column} is out of bounds')
-                continue
+
             return row, column
+
+    def _get_user_input(self, name):
+        """
+        Get the row or column number the user inputted.
+
+        :param name: the dimension (row or column) the number from the user is desired of
+        :type name: str
+        :return: the user's choice
+        :rtype: int
+        """
+        while True:
+            number = input(f'Player {self.mark}, enter a {name} number between 1 and 3: ')
+            try:
+                number = int(number)
+            except ValueError:
+                print('You did not enter a number')
+
+            if not 0 < number <= 3:
+                print(f'{name.capitalize()} {number} is out of bounds')
+                continue
+
+            return number - 1
