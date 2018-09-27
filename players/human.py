f3144dd14f3478a8338b35a0b056b3589f77de6f
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-26 22:36:17 -0400

    Add printing the board in case Human entered invalid number(s)

--- players/human.py
+++ players/human.py
@@ -15,8 +15,8 @@ class Human(Player):
         """
         while True:
             print(board)
-            row = self._get_user_input('row')
-            column = self._get_user_input('column')
+            row = self._get_user_input('row', board)
+            column = self._get_user_input('column', board)
 
             if not board.is_cell_empty(row, column):
                 print(f'The cell at row {row}, column {column} is not empty')
@@ -24,24 +24,30 @@ class Human(Player):
 
             return row, column
 
-    def _get_user_input(self, name):
+    def _get_user_input(self, name, board):
         """
         Get the row or column number the user inputted.
 
         :param name: the dimension (row or column) the number from the user is desired of
         :type name: str
+        :param board: the current board
+        :type board: Board
         :return: the user's choice
         :rtype: int
         """
         while True:
-            number = input(f'Player {self.mark}, enter a {name} number between 1 and 3: ')
+            number = input(
+                f'Player {self.mark}, enter a {name} number between 1 and 3: ')
             try:
                 number = int(number)
             except ValueError:
                 print('You did not enter a number')
+                print(board)
+                continue
 
             if not 0 < number <= 3:
                 print(f'{name.capitalize()} {number} is out of bounds')
+                print(board)
                 continue
 
             return number - 1
