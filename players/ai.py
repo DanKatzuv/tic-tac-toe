00192b5eaf092f5769a8b1d8dfe279370efa7ebc
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-09-27 22:20:22 -0400

    Add structure of AI player strategy

--- players/ai.py
+++ players/ai.py
@@ -8,9 +8,38 @@ class AI(Player):
         """
         Make a turn.
 
+        The strategy is taken from here: https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy
+
         :param board: the current game's board
         :type board: BoardRepresentation
         :return: choice of player
         :rtype: tuple
         """
-        pass
+        raise NotImplementedError
+
+    def _win(self):
+        raise NotImplementedError
+
+    def _block(self):
+        raise NotImplementedError
+
+    def _fork(self):
+        raise NotImplementedError
+
+    def _block_opponent_fork(self):
+        raise NotImplementedError
+
+    def _center(self):
+        raise NotImplementedError
+
+    def _opposite_corner(self):
+        raise NotImplementedError
+
+    def _empty_corner(self):
+        raise NotImplementedError
+
+    def _empty_side(self):
+        raise NotImplementedError
+
+    def _first_turn(self):
+        raise NotImplementedError
