46670e145e7f7969bd69456c99c4695d05624742
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-10-01 21:05:39 +0300

    Add method for handling second turn of the game for AI
    Note: This method is for handling the first turn of the second player.

--- players/ai.py
+++ players/ai.py
@@ -1,5 +1,9 @@
+from itertools import product
 from random import choice
+
+from board.board import Board
 from board.board_representation import BoardRepresentation
+from game import Game
 from .player import Player
 
 
@@ -67,6 +71,55 @@ class AI(Player):
         """
         if self._number_of_empty_cells(board) == 9:
             return choice((0, 2)), choice((0, 2))
+
+    def _second_turn(self, board):
+        """
+        Method that returns the choice of the case when the AI player plays the first turn.
+
+        The second player, who shall be designated "O", must respond to X's opening mark in such a way as to avoid
+        the forced win. Player O must always respond to a corner opening with a center mark, and to a center opening
+        with a corner mark. An edge opening must be answered either with a center mark, a corner mark next to the X,
+        or an edge mark opposite the X. Any other responses will allow X to force the win. Once the opening is
+        completed, O's task is to follow the above list of priorities in order to force the draw, or else to gain a
+        win if X makes a weak play.
+
+        More detailedly, to guarantee a draw, O should adopt the following strategies:
+        * If X plays corner opening move, O should take center, and then an edge, forcing X to block in the next
+        move. This will stop any forks from happening. When both X and O are perfect players and X chooses to start
+        by marking a corner, O takes the center, and X takes the corner opposite the original. In that case,
+        O is free to choose any edge as its second move. However, if X is not a perfect player and has played a
+        corner and then an edge, O should not play the opposite edge as its second move, because then X is not forced
+        to block in the next move and can fork.
+        * If X plays edge opening move, O should take center or one of the
+        corners adjacent to X, and then follow the above list of priorities, mainly paying attention to block forks.
+        * If X plays center opening move, O should take corner, and then follow the above list of priorities,
+        mainly paying attention to block forks.
+
+        :param board: current board
+        :type board: BoardRepresentation
+        :return: the choice of the first turn
+        :rtype: tuple
+        """
+
+        if self._number_of_empty_cells(board) == 8:
+            if not all((board.is_cell_empty(row, column) for row, column in  # if a corner opening occurred,
+                        product((0, 2), (0, 2)))):
+                return 1, 1  # fill the center
+            if not board.is_cell_empty(1, 1):  # if a center opening occured,
+                return choice((0, 2)), choice((0, 2))  # fill one of the corners
+
+            for row_number, row in enumerate(board):  # if an edge opening occurred, find location of inserted mark
+                if Game.FIRST_PLAYER_MARK in row:
+                    first_row, first_column = row, row.find(Game.FIRST_PLAYER_MARK)
+
+            choices = [(1, 1)]  # choices available if edge opening occurred
+            for dimension in (first_row, first_column):
+                if dimension in (1, 3):
+                    choices.append((dimension + 1, 1))
+                    choices.append((dimension - 1, 1))
+
+            return choice(choices)
+
     moves = (_first_turn, _second_turn, _win, _block, _fork, _block_opponent_fork,
              _center, _opposite_corner, _empty_corner, _empty_side)
 
