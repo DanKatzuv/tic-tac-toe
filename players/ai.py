bebbadd1647e6e87299100daef138a58dda41f86
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-10-01 19:55:21 +0300

    Add method that returns all the sequences in a board

--- players/ai.py
+++ players/ai.py
@@ -1,3 +1,4 @@
+from board.board_representation import BoardRepresentation
 from .player import Player
 
 
@@ -48,3 +49,24 @@ class AI(Player):
         raise NotImplementedError
     moves = (_first_turn, _second_turn, _win, _block, _fork, _block_opponent_fork,
              _center, _opposite_corner, _empty_corner, _empty_side)
+
+    @staticmethod
+    def _all_sequences(board):
+        """
+        Return all the sequences in a board.
+
+        A "sequence" is defined as every sequence of cells in a tic-tac-toe board, so "all sequences" is all the
+        rows, all the columns, the main diagonal and the secondary diagonal.
+
+        :param board: current board
+        :type: BoardRepresentation
+        :return: all the sequences in the board
+        :rtype: list
+        """
+        sequences = list()
+        sequences.append([row for row in board])  # rows
+        sequences.append(list(zip(*board)))  # columns
+        sequences.append([board[i][i] for i in range(3)])  # main diagonal
+        sequences.append([board[i][2 - i]
+                          for i in range(3)])  # secondary_diagonal
+        return sequences
