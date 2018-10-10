942b2522b123bc9d590a2c202454dc15d847c43c
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-10-10 09:37:37 +0300

    Add a more convenient representation of the board for a Human player

--- players/human.py
+++ players/human.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from .player import Player
 
 
@@ -13,8 +15,9 @@ class Human(Player):
         :return: choice of player
         :rtype: tuple
         """
+
         while True:
-            print(board)
+            print(self._new_board(board))
             cell = self._get_user_input(board)
             row, column = cell[0], cell[1]
             if not board.is_cell_empty(row, column):
@@ -32,6 +35,7 @@ class Human(Player):
         :return: the user's choice
         :rtype: tuple
         """
+        new_board = self._new_board(board)
         while True:
             number = input(
                 f'Player {self.mark}, enter a cell number between 1 and 9: ')
@@ -39,12 +43,31 @@ class Human(Player):
                 number = int(number)
             except ValueError:
                 print('You did not enter a number')
-                print(board)
+                print(new_board)
                 continue
 
             if not 0 < number <= 9:
                 print(f'Cell {number} is out of bounds')
-                print(board)
+                print(new_board)
                 continue
 
             return divmod(number - 1, 3)
+
+    @staticmethod
+    def _new_board(board):
+        """
+        :param board: current board
+        :type board: BoardRepresentation
+        :return: a more convenient board representation for a human
+        :rtype: list[list]
+        """
+        new_board = [['', '', ''],
+                     ['', '', ''],
+                     ['', '', '']]
+        for row, column in product(range(3), range(3)):
+            if board.is_cell_empty(row, column):
+                to_insert = str(row * 3 + column + 1)
+            else:
+                to_insert = board.rows[row][column]
+            new_board[row][column] = to_insert
+        return ''.join(' | '.join(row) + '\n' for row in new_board)
